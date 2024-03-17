7adc057d05d0dc4e67cd8f6aba3af2222323b26f
Suraj <surajsa59@gmail.com>
2024-03-17 11:34:09 +0000

    Adding Jump and Double jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,33 +4,81 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jump;
-    public Rigidbody2D rb;
-    private float movingInput;
+    // Variables for controlling player movement and physics
+    public float moveSpeed; // Speed at which the player moves horizontally
+    public float jumpPower; // add a jump power in player script
+    public Rigidbody2D rb; // Reference to the player's Rigidbody component
 
-    public LayerMask whatIsGround;
-    public float groundCheckDistance;
-    private bool isGrounded;
+    [SerializeField]private bool canDoubleJump;
+    [SerializeField]private float movingInput; // Input value for horizontal movement
+
+    // Variables for checking ground collision
+    public LayerMask whatIsGround; // Layer mask for identifying ground
+    public float groundCheckDistance; // Distance to check for ground collision
+    [SerializeField]private bool isGrounded; // check if the player is grounded
     
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics2D.Raycast(transform.position,Vector2.down,groundCheckDistance,whatIsGround);
+        // Check for ground collision
+        collisionCheck();
         
+        // Read horizontal movement input from player
         movingInput = Input.GetAxisRaw("Horizontal");
 
+        // Check if the player pressed the jump button
         if(Input.GetKeyDown(KeyCode.Space)){
-            rb.velocity = new Vector2(rb.velocity.x,jump);
+
+            // If the player is grounded, allow them to jump
+            JumpButton();
         }
-        rb.velocity = new Vector2(moveSpeed * movingInput ,rb.velocity.y);
+
+        if(isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        // Move the player horizontally
+        Move();
     }
 
-    
+    private void JumpButton()
+    {
+        if(isGrounded)
+        {
+            Jump();
+        }
+        else if(canDoubleJump)
+        {
+            canDoubleJump = false;
+            Jump();
+        }
+    }
+
+    // Function to handle horizontal movement
+    private void Move()
+    {
+         // Set the player's velocity based on the input and current velocity
+         rb.velocity = new Vector2(moveSpeed * movingInput ,rb.velocity.y);
+    }
+
+    // Function to handle player jumping
+    private void Jump()
+    {
+        // Apply vertical force to make the player jump
+        rb.velocity = new Vector2(rb.velocity.x,jumpPower);
+    }
+
+    // Function to check for ground collision
+    private void collisionCheck()
+    {
+        // Cast a ray downwards to check for ground collision
+        // If the ray hits an object in the whatIsGround layer mask within the specified distance, set isGrounded to true
+        isGrounded = Physics2D.Raycast(transform.position,Vector2.down,groundCheckDistance,whatIsGround);
+    }
 }
