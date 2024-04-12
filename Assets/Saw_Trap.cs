4e08c8232fc86964c25ca9d8e58f75900f07ed32
Suraj <surajsa59@gmail.com>
2024-04-12 22:19:33 +0100

    Animated the saw trap

--- Assets/Saw_Trap.cs
+++ Assets/Saw_Trap.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Saw_Trap : Trap
 {
     private Animator anim;
-    [SerializeField] private bool isWorking;
+    private bool isWorking;
     [SerializeField] private Transform[] movePoint;
     [SerializeField] private float speed;
     private int movePointIndex;
+    private float cooldownTimer;
+    [SerializeField] private float cooldown;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +22,23 @@ public class Saw_Trap : Trap
     // Update is called once per frame
     void Update()
     {
+        cooldownTimer-=Time.deltaTime;
+        bool isWorking = cooldownTimer < 0;
         anim.SetBool("isWorking",isWorking);
-        transform.position = Vector3.MoveTowards(transform.position,movePoint[movePointIndex].position,speed * Time.deltaTime);
 
-        if(Vector2.Distance(transform.position,movePoint[0].position) < 0.15f)
+        if(isWorking)
         {
+            transform.position = Vector3.MoveTowards(transform.position,movePoint[movePointIndex].position,speed * Time.deltaTime);
+        }
+        if(Vector2.Distance(transform.position,movePoint[movePointIndex].position) < 0.15f)
+        {
+            cooldownTimer = cooldown;
             movePointIndex++;
+
+            if(movePointIndex >= movePoint.Length)
+            {
+                movePointIndex = 0;
+            }
         }
     }
 }
