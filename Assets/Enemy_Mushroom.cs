74ac01dc3b1a46961e3c38dfb17587fc9ae53070
Suraj <surajsa59@gmail.com>
2024-04-15 20:55:33 +0100

    adding the mushroom enemy

--- Assets/Enemy_Mushroom.cs
+++ Assets/Enemy_Mushroom.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class Enemy_Mushroom : Enemy
 {
     [SerializeField] private float speed;
+    [SerializeField] private float idleTime = 2;
+    [SerializeField] private float idleTimeCounter;
 
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+        facingDirection = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(speed, rb.velocity.y);
+        if(idleTimeCounter <= 0)
+        {
+            rb.velocity = new Vector2(speed *facingDirection, rb.velocity.y);
+        }
+
+        idleTimeCounter -= Time.deltaTime;
+        CollisionChecks();
+
+        if(wallDetected || !groundDetected)
+        {
+            Flip();
+            idleTimeCounter = idleTime;
+        }
+
+        anim.SetFloat("xVelocity",rb.velocity.x);
     }
 }
